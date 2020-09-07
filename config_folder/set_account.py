ea3f914c7ba348dc66bbcfb674159c5ca8fe97a5
CSFeng0826 <Fongwith87592@gmail.com>
2020-09-07 18:59:19 +0800

    add generate password file and auto set password script

--- config_folder/set_account.py
+++ config_folder/set_account.py
@@ -5,13 +5,14 @@ import pandas as pd
 df = pd.read_csv("account.csv", encoding='utf-8')
 
 nids = df.username.to_list()
+passwds = df.passwd.to_list()
 
 pwd='chishong656123'
 
 for nid in nids:
     call('echo {} | sudo -S {}'.format(pwd, "docker exec -it jupyterhub useradd --create-home " + nid), shell=True)
-
-call('echo {} | sudo -S {}'.format(pwd, "docker exec --user admin jupyterhub python add_user.py"), shell=True)
+    cmd = "docker exec --user admin jupyterhub bash -c \"echo -e '" + passwds[count] + "\\n" + passwds[count] + "' | passwd " + nids[count] + "\""
+    call('echo {} | sudo -S {}'.format(pwd, cmd), shell=True)
 
 for nid in nids:
     call('echo {} | sudo -S {}'.format(pwd, "docker exec --user " + nid + " jupyterhub pip install --user pandas"), shell=True)
